2397743c67ffb9953f31e674bf8327b666b1affa
Alex Kovalev <avkovalevs@gmail.com>
2019-11-27 20:04:07 +0000

    Added RS scripts

---
+++ roles/asgspot/files/addinstance.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+### Don't start this script on ansible, PG master and PG special slave
+### This script must be started only on the regular slaves at AWS Auto Scaling Group 
+### All actions will be logged to file /home/ubuntu/log.out
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/home/ubuntu/log.out 2>&1
+
+###Get private  ip
+private_ip=$(/usr/bin/curl http://169.254.169.254/latest/meta-data/local-ipv4)
+echo $private_ip
+
+### Connect to ansible master and run commands: add ip to /etc/ansible/hosts and run playbook which will change the clone to a new values
+/usr/bin/ssh ubuntu@172.31.41.41 << EOF
+if [[ $(grep -n "$private_ip" /etc/ansible/hosts | wc -l) == 0 ]]; then
+  echo "$private_ip" | sudo tee -a /etc/ansible/hosts
+  cd /etc/ansible
+  /usr/bin/ansible-playbook -i hosts playbook.yml -v 
+else
+  echo "$private_ip already exist in hosts file"
+fi
+EOF
---
+++ roles/asgspot/files/dropinstance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###Get private  ip
+private_ip=$(/usr/bin/curl http://169.254.169.254/latest/meta-data/local-ipv4)
+echo $private_ip
+
+### Connect to ansible master and run commands: drop ip from /etc/ansible/hosts and run playbook which will remove ip from /etc/pgpool/pgpool.conf
+/usr/bin/ssh ubuntu@172.31.41.41 << EOF
+if [[ $(grep -n "$private_ip" /etc/ansible/hosts | wc -l) > 0 ]]; then
+  sed -i "/$private_ip/d" /etc/ansible/hosts
+  cd /etc/ansible
+  /usr/bin/ansible-playbook -i hosts playbook.yml -v 
+else
+  echo "$private_ip already exist in hosts file"
+fi
+EOF
